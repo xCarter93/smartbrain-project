59d5a2f5443e2811b0e644d0d3422b39051cb628
Patrick Carter <xCarter93@gmail.com>
2023-05-30 21:47:52 -0400

    Fixed links to reference render URL

--- src/App.js
+++ src/App.js
@@ -72,7 +72,7 @@ class App extends Component {
     const { input } = this.state;
     this.setState({ imageURL: input });
 
-    fetch("http://localhost:3000/imageurl", {
+    fetch("https://smart-brain-api-270t.onrender.com/imageurl", {
       method: "post",
       headers: { "Content-Type": "application/json" },
       body: JSON.stringify({ input: input }),
@@ -80,7 +80,7 @@ class App extends Component {
       .then((response) => response.json())
       .then((result) => {
         if (result) {
-          fetch("http://localhost:3000/image", {
+          fetch("https://smart-brain-api-270t.onrender.com/image", {
             method: "put",
             headers: { "Content-Type": "application/json" },
             body: JSON.stringify({ id: this.state.user.id }),
